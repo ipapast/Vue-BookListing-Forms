5eb4c7d4d3a0cc587183753d82cfdd1b27c27eef
Ilianna Papastefanou <ilipapast@gmail.com>
2020-10-24 16:01:44 +0100

    Refactor BookForm Component to use BookData object

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,27 +1,39 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div>
+      <label for="finishedReading">Finished Reading</label>
+      <input v-model="bookData.finishedReading" type="checkbox" id="finishedReading">
+      <label for="borrowed">borrowed</label>
+      <input v-model="bookData.ownership" type="radio" value="borrowed" id="borrowed">
+      <label for="bought">bought</label>
+      <input v-model="bookData.ownership" type="radio" value="bought" id="bought">
+    </div>
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>
 
 <script>
-export default {
-  name: "BookForm",
-  props: ["books"],
-  data() {
-    return {
-      bookTitle: "",
-      bookAuthor: ""
-    };
-  },
-  methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+  export default {
+    name: "BookForm",
+    props: ["books"],
+    data() {
+      return {
+        bookData: {
+          bookTitle: "",
+          bookAuthor: "",
+          finishedReading: false,
+          ownership: []
+        }
+      }
+    },
+    methods: {
+      bookSubmit(bookData) {
+        this.$emit("addBook", bookData);
+      }
     }
-  }
-};
+  };
 </script>
 
 <style>
