37d6d8b2d8f8be797bb09e13731f0d2174c1acd6
Ilianna Papastefanou <ilipapast@gmail.com>
2020-10-24 16:13:39 +0100

    Put v-if and else in the root element

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,7 +1,8 @@
 <template>
-  <li>{{book.title}} : {{book.author}}</li>
-  <span v-if="book.finishedReading === true">Read</span>
-  <span v-else>Not Read</span>
+  <li>{{book.title}} : {{book.author}}
+    <span v-if="book.finishedReading === true">Read</span>
+    <span v-else>Not Read</span>
+  </li>
 </template>
 
 <script>
