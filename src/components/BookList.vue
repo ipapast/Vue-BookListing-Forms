6a91409a30cfe3f3e8808119d4bbf15e10cc1478
Ilianna Papastefanou <ilipapast@gmail.com>
2020-10-24 19:59:23 +0100

    Add search functionality

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input v-model="searchInput" type="text" placeholder="Search Books">
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <hr>
     <h2>Filtered Books By Ownership</h2>
@@ -31,6 +32,7 @@
         states: ["Want to Read", "Read", "Reading"],
         filters: ["bought", "borrowed"],
         holding: "bought",
+        searchInput: "",
         books: [
           {title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true, ownership: "borrowed"},
           {title: "American Gods", author: "Neil Gaiman", finishedReading: false, ownership: "bought"},
@@ -45,6 +47,12 @@
     computed: {
       filteredBooks() {
         return _.filter(this.books, ["ownership", this.holding]);
+      },
+      searchedBooks() {
+        const searchFilter = book => {
+          return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+        };
+        return _.filter(this.books, searchFilter);
       }
     },
     methods: {
@@ -57,7 +65,8 @@
         });
       }
     }
-  };
+  }
+  ;
 </script>
 
 <style>
